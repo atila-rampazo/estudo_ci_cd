0379f2d4cb97f83a85d64e7707a54187c86cd985
Atila Rampazo <atila.rampazo@dextra-sw.com>
2021-12-21 16:49:12 -0300

    feat: ajustado script de CD para execucao do bash

--- config_cd.sh
+++ config_cd.sh
@@ -5,56 +5,56 @@ FILE_ENV=.env
 if [ ! -f "$FILE" ]; then
     echo $1 | sudo -S docker-compose exec -T curso_api_cache php -r "file_exists('.env') || copy('.env.example', '.env');"
 fi
-    echo $1 | sudo -S docker-compose exec -T curso_api_cache cat > .env <<EOF
+    echo $1 | sudo -S docker-compose exec -T curso_api_cache cat  > .env <<- "EOF"
     APP_NAME="Api Cache"
-APP_ENV=production
-APP_KEY=
-APP_DEBUG=false
-APP_URL="https://minharendapassiva.com.br"
+    APP_ENV=production
+    APP_KEY=
+    APP_DEBUG=false
+    APP_URL="https://minharendapassiva.com.br"
 
-LOG_CHANNEL=stack
-LOG_LEVEL=debug
+    LOG_CHANNEL=stack
+    LOG_LEVEL=debug
 
-DB_CONNECTION=mysql
-DB_HOST=$2
-DB_PORT=3306
-DB_DATABASE=$3
-DB_USERNAME=$4
-DB_PASSWORD=$5
+    DB_CONNECTION=mysql
+    DB_HOST=$2
+    DB_PORT=3306
+    DB_DATABASE=$3
+    DB_USERNAME=$4
+    DB_PASSWORD=$5
 
-BROADCAST_DRIVER=log
-CACHE_DRIVER=redis
-FILESYSTEM_DRIVER=local
-QUEUE_CONNECTION=redis
-SESSION_DRIVER=redis
-SESSION_LIFETIME=120
+    BROADCAST_DRIVER=log
+    CACHE_DRIVER=redis
+    FILESYSTEM_DRIVER=local
+    QUEUE_CONNECTION=redis
+    SESSION_DRIVER=redis
+    SESSION_LIFETIME=120
 
-MEMCACHED_HOST=127.0.0.1
+    MEMCACHED_HOST=127.0.0.1
 
-REDIS_HOST=127.0.0.1
-REDIS_PASSWORD=null
-REDIS_PORT=6379
+    REDIS_HOST=127.0.0.1
+    REDIS_PASSWORD=null
+    REDIS_PORT=6379
 
-MAIL_MAILER=smtp
-MAIL_HOST=mailhog
-MAIL_PORT=1025
-MAIL_USERNAME=null
-MAIL_PASSWORD=null
-MAIL_ENCRYPTION=null
-MAIL_FROM_ADDRESS=null
-MAIL_FROM_NAME="${APP_NAME}"
+    MAIL_MAILER=smtp
+    MAIL_HOST=mailhog
+    MAIL_PORT=1025
+    MAIL_USERNAME=null
+    MAIL_PASSWORD=null
+    MAIL_ENCRYPTION=null
+    MAIL_FROM_ADDRESS=null
+    MAIL_FROM_NAME="${APP_NAME}"
 
-AWS_ACCESS_KEY_ID=
-AWS_SECRET_ACCESS_KEY=
-AWS_DEFAULT_REGION=us-east-1
-AWS_BUCKET=
-AWS_USE_PATH_STYLE_ENDPOINT=false
+    AWS_ACCESS_KEY_ID=
+    AWS_SECRET_ACCESS_KEY=
+    AWS_DEFAULT_REGION=us-east-1
+    AWS_BUCKET=
+    AWS_USE_PATH_STYLE_ENDPOINT=false
 
-PUSHER_APP_ID=
-PUSHER_APP_KEY=
-PUSHER_APP_SECRET=
-PUSHER_APP_CLUSTER=mt1
+    PUSHER_APP_ID=
+    PUSHER_APP_KEY=
+    PUSHER_APP_SECRET=
+    PUSHER_APP_CLUSTER=mt1
 
-MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"
-MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"
-EOF
+    MIX_PUSHER_APP_KEY="${PUSHER_APP_KEY}"
+    MIX_PUSHER_APP_CLUSTER="${PUSHER_APP_CLUSTER}"
+    EOF
